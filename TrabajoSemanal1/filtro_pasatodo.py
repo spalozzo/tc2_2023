f727ba9b2ec1ac4f56fb63d8ee9e0cdcce851e2a
santi <spalozzo@frba.utn.edu.ar>
2023-04-20 01:26:44 -0300

    ts1_v2

--- TrabajoSemanal1/filtro_pasatodo.py
+++ TrabajoSemanal1/filtro_pasatodo.py
@@ -15,16 +15,20 @@ import numpy as np
 # Librería de TC2
 from pytc2.sistemas_lineales import pzmap, GroupDelay, bodePlot
 
-R1= 1000; R2=1000; R3=1000; C=1e-6;
+#Valores desnormalizados
+#R1= 1000; R2=1000; R3=1000; C=1e-6;
+#Valores normalizados
+R1= 1000; R2=R1; R3=R1; C=1/R1;
 
-w0 = 1/(R3*C)
+#w0 = 1/(R3*C)
+w0 = 1
 
-my_tf = TransferFunction( [1, -(R2/R1)*w0], [1, w0] )
+my_tf_norm = TransferFunction( [1, -(R2/R1)*w0], [1, w0] )
 
 plt.close('all')
 
-bodePlot(my_tf, fig_id=1, filter_description = 'Pasatodo')
+bodePlot(my_tf_norm, fig_id=1, filter_description = 'Pasatodo')
 
-pzmap(my_tf, fig_id=2, filter_description = 'Pasatodo') #S plane pole/zero plot
+pzmap(my_tf_norm, fig_id=2, filter_description = 'Pasatodo') #S plane pole/zero plot
 
-GroupDelay(my_tf, fig_id=3, filter_description = 'Pasatodo')
+GroupDelay(my_tf_norm, fig_id=3, filter_description = 'Pasatodo')
